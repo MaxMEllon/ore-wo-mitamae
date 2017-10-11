051110c9d58cae785afb730fee23ad1dbcd7dbcc
MaxMEllon <maxmellon1994@gmail.com>
2017-10-11 06:23:13 +0000

    :smile:

--- cookbooks/fzf/default.rb
+++ cookbooks/fzf/default.rb
@@ -3,7 +3,6 @@ execute 'get binary by fzf-bin' do
     wget https://github.com/junegunn/fzf-bin/releases/download/0.17.0/fzf-0.17.0-linux_amd64.tgz \
       -O ~/local/bin/fzf.tgz
     tar xf fzf.tgz
-    rm fzf.tgz
   EOF
 
   cwd "#{node[:env][:home]}/local/bin"
