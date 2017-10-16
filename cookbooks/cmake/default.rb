dd7ccfbb2b971e91ea09f0c2239832b88d87e30a
MaxMEllon <maxmellon1994@gmail.com>
2017-10-17 03:26:20 +0900

    Fix path

--- cookbooks/cmake/default.rb
+++ cookbooks/cmake/default.rb
@@ -2,7 +2,7 @@ execute 'install cmake' do
   command <<-EOF
     wget https://cmake.org/files/v3.10/cmake-3.10.0-rc2.tar.gz
     tar xf cmake-3.10.0-rc2.tar.gz
-    cd cmake-3.10.0-rc2.tar.gz
+    cd cmake-3.10.0-rc2
     ./configure
     make
     sudo make install
